333a2e5a152fc7c1f02605e9017f1cd28df5cc80
Ian-Polito <Ian-Polito@users.noreply.github.com>
2014-11-11 15:53:07 -0800

    updated to add skeleton methods for commands
    also adds a few various other things

--- SafeWalkServer.java
+++ SafeWalkServer.java
@@ -1,7 +1,7 @@
 /*
  Project 5
  Safe Walk Server
- Ian Polito & Kurt Sermersheim
+ Ian Polito ipolito@purdue.edu
  CS 180
  11/10/14
  */
@@ -9,11 +9,13 @@
 import java.util.Scanner;
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
 
 public class SafeWalkServer {
     
     int portNumber = 8888;
     ServerSocket ss;
+    ArrayList requests = new ArrayList();
     
     public SafeWalkServer(int port) throws SocketException, IOException { //constructs the server if given a port #
         if (port >= 1025 && 65535 >= port) {
@@ -44,6 +46,16 @@ public class SafeWalkServer {
             Socket client = ss.accept();
         }
     }
+    //Here begins the methods that will be invoked via 'commands'
+    public static void listRequests() {//will need some sort of input to tell which client it needs to send to
+        //iterate through the arraylist and print the requests
+    }
+    public static void reset() {//will need some sort of input to tell which client it needs to send to
+        //close up the clients' request and tell them that an error occured with the connection
+    }
+    public static void shutdown() {//will need some sort of input to tell which client it needs to send to
+        //close up, similar to the reset command, but exit the run loop, shuting down the server
+    }
     
     public static void main(String[] args) throws SocketException, IOException {
         if (args.length == 0) {
@@ -62,3 +74,4 @@ public class SafeWalkServer {
         }
     }
 }
+
