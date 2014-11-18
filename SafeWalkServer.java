0922bba169c656ccaa3696faca8d8d95b0c2f653
Ian-Polito <Ian-Polito@users.noreply.github.com>
2014-11-17 18:53:35 -0800

    added a buttload of stuff

--- SafeWalkServer.java
+++ SafeWalkServer.java
@@ -1,28 +1,30 @@
 /*
  Project 5
  Safe Walk Server
- Ian Polito ipolito@purdue.edu
+ @author Ian Polito ipolito 801
+ @author Kurt Sermersheim ksermer lab section
  CS 180
- 11/10/14
  */
 
 import java.util.Scanner;
 import java.net.*;
 import java.io.*;
 import java.util.ArrayList;
-
-interface Runnable {
-}
-
+import java.util.Arrays;
+import java.util.List;
 public class SafeWalkServer implements Runnable {
     
     int portNumber = 8888; //default listening port
     ServerSocket ss; //the server socket
-    ArrayList requests = new ArrayList(); //list of requests for SafeWalk
+    ArrayList<List> requests = new ArrayList<>(); //list of arrayLists ???
+    List<String> elements; //list of elements of requests
     ArrayList clients = new ArrayList(); //list of client objects
     PrintWriter out; //output to client
     BufferedReader in; //input from client
     boolean isStopped = false; //variable used to stop the 'run' loop if necessary
+    
+    Socket difclient; //just initializes the client
+    final ArrayList<Socket> clientsConnected = new ArrayList<>();
     
     public SafeWalkServer(int port) throws SocketException, IOException { //constructs the server if given a port #
         if (port >= 1025 && 65535 >= port) {
@@ -38,6 +40,10 @@ public class SafeWalkServer implements Runnable {
         }
     }
     
+    public SafeWalkServer(Socket difclient) {
+    	this.difclient = difclient;
+    }
+    
     public SafeWalkServer() throws SocketException, IOException { //constructs the server if not given a port #
         System.out.println("Port not specified, using free port 8888.");
         ss = new ServerSocket(portNumber);
@@ -45,36 +51,81 @@ public class SafeWalkServer implements Runnable {
     }
     
     public int getLocalPort() { // returns the port number we are on
-        return portNumber;
+        return ss.getLocalPort();
     }
     
     public void run() throws SocketException, IOException { //Start a loop to accept incoming connections
         while (!isStopped) {
             Socket client = ss.accept();
-            //new Thread(client).start();
+            clientsConnected.add(client);
+            System.out.println("Client Connected.");
             out = new PrintWriter(client.getOutputStream(), true);
             out.flush();
             in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            Client c = new Client(in.readLine(), client);
+            String command = in.readLine();
+            
+            if (command.startsWith(":")) {
+            	if (command.equals(":LIST_PENDING_REQUESTS")) { //lists pending requests
+            		listRequests(client);
+            	}
+            
+            	if (command.equals(":SHUTDOWN")) { //shuts down the server
+            		shutdown(client);
+            	}
+            
+            	if (command.equals(":RESET")) { //resets the server
+            		reset(client);
+            	}
+            	else {
+            		out.println("ERROR: invalid request"); //sends a message saying it was an invalid command
+            		client.close();
+            	}
+            }
+            //Thread thread = new Thread(ss2);
+            //thread.start();
+            else {
+            	elements = Arrays.asList(command.split(","));
+            	if (elements.size() != 4) {
+            		out.println("ERROR: invalid request");
+            		client.close();
+            	}
+            	else if (!elements.get(1).equals("CL50") | !elements.get(1).equals("EE") | !elements.get(1).equals("LWSN") 
+            			| !elements.get(1).equals("PMU") | !elements.get(1).equals("PUSH") | elements.get(1).equals("*")) {
+            		out.println("ERROR: invalid request");
+            		client.close();
+            	}
+            }
         }
     }
     
     //Here begins the methods that will be invoked via 'commands'------------------------------------------------------
     
-    public void listRequests() { //will need some sort of input to tell which client it needs to send to
+    public void listRequests(Socket client) throws SocketException, IOException { //will need some sort of input to tell which client it needs to send to
         //iterate through the arraylist and print the requests
-        for (int i = 0; i < requests.size(); i++) {
-            System.out.println(requests.get(i));
-        }
+        out.println(requests);
+        out.flush();
+        client.close();
     }
     
-    public void reset() { //will need some sort of input to tell which client it needs to send to
+    public void reset(Socket client) throws SocketException, IOException { //will need some sort of input to tell which client it needs to send to
         //close up the clients' request and tell them that an error occured with the connection
-        
+    	out.println("RESPONSE: success");
+    	out.flush();
+    	client.close();
+    	for (int i = 0; i < clientsConnected.size(); i++) {
+    		out = new PrintWriter(clientsConnected.get(i).getOutputStream(), true);
+    		out.println("ERROR: connection reset");
+    		clientsConnected.get(i).close();
+    	}
+    	requests.clear();
+    	elements.clear();
+    	clientsConnected.clear();
     }
     
-    public void shutdown() { //will need some sort of input to tell which client it needs to send to
+    public void shutdown(Socket client) throws SocketException, IOException { //will need some sort of input to tell which client it needs to send to
         //close up, similar to the reset command, but exit the run loop, shuting down the server
+    	reset(client);
+    	isStopped = true;
     }
     //Here ends the 'command' methods----------------------------------------------------------------------------------
     
@@ -95,14 +146,14 @@ public class SafeWalkServer implements Runnable {
         }
     }
 }
-
-class Client {
+//might need this class still? Who knows, this project is a holy mess.
+//class Client {
     
-    String command;
-    Socket client;
+    //String command;
+    //Socket client;
     
-    public Client(String command, Socket client) {
-        this.command = command;
-        this.client = client;
-    }
-}
+    //public Client(Socket client, String command) {
+        //this.command = command;
+        //this.client = client;
+    //}
+//}
